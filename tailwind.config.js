f9c186f31aba9f32f49664fba6bb4a4e82e16f55
birdkr <birdkr@gmail.com>
2026-04-07 15:32:05 +0900

    New 이슈 마크 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.html", "./static/src/**/*.js"],
+  safelist: [
+    "bg-error/5",
+    "bg-error",
+    "bg-warning/10",
+    "bg-info/5",
+  ],
   theme: {
     extend: {},
   },
